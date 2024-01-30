f03e8e45c5771d2a00e7aba86f486c84677570ec
zz.007 <39936133+bobbyz007@users.noreply.github.com>
2024-01-30 17:11:38 +0800

    Delete buildSrc/build directory

--- buildSrc/build/kotlin-dsl/plugins-blocks/extracted/myproject.library-conventions.gradle.kts
+++
@@ -1,9 +0,0 @@
-                                                         
-                                                                                           
-
-plugins {
-    `java-library`
-    `maven-publish`
-    id("version-catalog")
-    id("myproject.java-conventions")
-}
